9bc06c8f9400a0ed2fbd90b900c23f9b7c7d0976
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2022-01-22 20:26:48 +0400

    added input number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Enter your number");
+
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("Reading the string failed");
+
+    println!("You was guess: {}", guess);
 }
