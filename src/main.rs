95e0ceb795a7734f9938e178bb5f8aabf7d24a94
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2022-01-23 17:29:12 +0400

    added main loop for trying untill guess

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,27 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("Secret number is {}", secret_number);
+    loop{
+        println!("Enter your number");
 
-    println!("Enter your number");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Reading the string failed");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Reading the string failed");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please, enter the number");
+        println!("You was guess: {}", guess);
 
-    println!("You was guess: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Secret number is bigger than your"),
-        Ordering::Greater => println!("Secret number is smaller than your"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Secret number is bigger than your"),
+            Ordering::Greater => println!("Secret number is smaller than your"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
